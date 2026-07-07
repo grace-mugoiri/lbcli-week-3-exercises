9f10f69727b8d1f98e80cee8277b9ebfee88992f
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-07 21:56:02 +0300

    submit 2 and 3

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,11 @@
 # Create a native segwit address and get the public key from the address.
-address=$(bitcoin-cli -regtest getnewaddress address_type=p2sh-segwit)
-pubkey=$(bitcoin-cli -regtest -rpcwallet=builderswallet validateaddress $address | jq -r '.pubkey')
+WALLET_NAME="builderswallet"
+bitcoin-cli -regtest -rpcwallet="$WALLET_NAME" loadwallet "$WALLET_NAME" 2>/dev/null || \
+    bitcoin-cli -regtest createwallet "$WALLET_NAME" 2>/dev/null
+
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet="$WALLET_NAME" getnewaddress "" "bech32")
+
+ADDRESS_INFO=$(bitcoin-cli -regtest -rpcwallet="$WALLET_NAME" getaddressinfo $ADDRESS)
+
+PUBLIC_KEY=$(echo $ADDRESS_INFO | jq -r '.pubkey')
+echo "$PUBLIC_KEY"
