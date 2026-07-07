4f631e9840c62732d778195aaa6cfdb971ddbd99
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-07 22:23:45 +0300

    submit 6 and 7

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,12 @@
 # What is the hash of this partially signed transaction?
-# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+WALLET_NAME="builderswallet"
+
+bitcoin-cli -regtest loadwallet "$WALLET_NAME" 2>/dev/null || \
+bitcoin-cli -regtest createwallet "$WALLET_NAME" 2>/dev/null
+
+transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+DECODED=$(bitcoin-cli -regtest -rpcwallet="$WALLET_NAME" decodepsbt "$transaction")
+
+HASH=$(echo "$DECODED" | jq -r '.tx.txid')
+
+echo "$HASH"
