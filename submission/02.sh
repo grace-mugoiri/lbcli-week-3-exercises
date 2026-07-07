01bce8095bf0f7f3befc1a704ffff4a4762114ef
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-07 21:30:45 +0300

    create wallet and a segwit wallet

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+address=$(bitcoin-cli -regtest getnewaddress address_type=p2sh-segwit)
+pubkey=$(bitcoin-cli -regtest -rpcwallet=builderswallet validateaddress $address | jq -r '.pubkey')
